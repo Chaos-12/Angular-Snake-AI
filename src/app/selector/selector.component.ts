f5ec39c6ae64c98af5bb156618bea66ebd85a758
David Iglesias <dvd.iglesias@outlook.com>
2022-02-04 17:51:13 +0100

    Selector component

--- src/app/selector/selector.component.ts
+++ src/app/selector/selector.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { ViewUserComponent } from '../view-user/view-user.component';
 
 @Component({
   selector: 'app-selector',
@@ -6,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selector.component.css']
 })
 export class SelectorComponent implements OnInit {
+  menu = [
+    { text: 'Home', icon: 'home', component: HomeComponent },
+    { text: 'User', icon: 'calculator', component: ViewUserComponent },
+  ]
+  actual: any = this.menu[0].component;
 
   constructor() { }
+
+  public select(index: number): void {
+    this.actual = this.menu[index].component;
+  }
 
   ngOnInit(): void {
   }
