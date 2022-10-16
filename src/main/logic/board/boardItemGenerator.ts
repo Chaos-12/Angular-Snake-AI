32a48196bf9091fffcfaebe400675178c469e861
Chaos-12 <dvd.iglesias@outlook.com>
2022-10-17 01:12:01 +0200

    removed old code to generate food

--- src/main/logic/board/boardItemGenerator.ts
+++ src/main/logic/board/boardItemGenerator.ts
@@ -3,18 +3,6 @@ import { Position, Board, PositionGenerator } from "src/main/logic";
 export class BoardItemGenerator {
 
   public static generateRandomFood(board:Board):void{
-    /*let generated = false;
-    let tries = 0;
-    while(!generated && tries < 3){
-      let x = Math.ceil(Math.random()*board.width);
-      let y = Math.ceil(Math.random()*board.height);
-      let newPosition = new Position(x,y);
-      if(!board.snake.contains(newPosition)){
-        board.food = newPosition;
-        generated = true;
-      }
-      tries ++;
-    }*/
     let foodPositions = new PositionGenerator();
     foodPositions.addException(board.snake.head);
     board.snake.body.forEach(part => {
